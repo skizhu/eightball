557fd9f52fc03cdadac43acfdf8634651d1e4562
skizhu <69230144+skizhu@users.noreply.github.com>
2020-08-07 03:29:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+options = ["As I see it, yes.", " Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", " It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes - definitely.", "You may rely on it."]
+
+
+function choose(){
+	var eightballA = document.getElementById("eightballA");
+	eightballA.id = "eightballB";
+
+	var choice = Math.floor(Math.random() * 20);
+    var randomChoice = options[choice];
+
+    console.log(options[19]);
+
+	var result = document.getElementById("eightballB").append(randomChoice);
+}
+
+function restart(){
+	location.reload();
+}
